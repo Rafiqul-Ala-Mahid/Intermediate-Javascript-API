e47c94fe813460c14b38f5e2414c64c4bb751d62
MacBook <macbook@MacBooks-Air.Dlink>
2022-09-05 03:48:25 +0600

    third commit

--- js/json.js
+++ js/json.js
@@ -1,19 +1,31 @@
-const url =`https://openapi.programming-hero.com/api/news/category/01`
-fetch(url)
-    .then(res => res.json())
-    .then(data => loaddata(data.data))
+const loadNews=async()=>{
+    const link = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(link)
+    const data = await res.json()
+    displayData(data.data.news_category)
+}
+loadNews()
+function displayData(data) {
+    const head = document.getElementById('header')
+    data.forEach(element => {
+        // console.log(element.category_name)
+        const a = document.createElement('a')
+        a.innerHTML = `
+            <a href="#" class="anchor mx-4" onclick="loading(${element.category_id})">${element.category_name}</a>
+        `
+        head.appendChild(a)
+    });
+}
+function loading(id) {
+    console.log(id)
+    const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => loaddata(data.data))
+}
 
-// function displayData(data) {
-//     const div = document.getElementById('header')
-//     data.forEach(element => {
-//         const a=document.createElement('a')
-//         console.log(element)
-//         a.innerHTML = `
-//             <a href="#"class="px-4 anchor">${element.category_name}</a>
-//         `
-//         div.appendChild(a)
-//     });
-// }
+
+
 const loaddata = (data) => {
     const div = document.getElementById('news-container')
     data.forEach(element => {
@@ -21,7 +33,7 @@ const loaddata = (data) => {
         const news = document.createElement('div')
         news.innerHTML = `
             <div class="row g-2 my-4">
-                    <div class="col-md-2">
+                    <div class="col-md-2 mx-3">
                         <img src="${element.thumbnail_url}" class="img-fluid rounded-start" alt="...">
                     </div>
                     <div class="col-md-8">
@@ -34,4 +46,5 @@ const loaddata = (data) => {
         `
         div.appendChild(news)
     });
+    news.innerHTML=``
 }
